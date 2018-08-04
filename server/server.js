6d742600ed612043d7c1724a917d011b725f132f
janardan96 <jdcodemon@gmail.com>
2018-08-04 15:25:55 +0530

    new message and create message

--- server/server.js
+++ server/server.js
@@ -1,27 +1,34 @@
-const express=require("express");
-const path=require("path");
-const socketIO=require("socket.io");
-const http=require("http");
+const express = require("express");
+const path = require("path");
+const socketIO = require("socket.io");
+const http = require("http");
 
-const publicPath=path.join(__dirname,"../public");
-const port=process.env.PORT ||3000
-var app=express();
-var server=http.createServer(app);
-var io=socketIO(server);
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 
-io.on("connection",(socket)=>{
-console.log("New User is connected");
+io.on("connection", (socket) => {
+    console.log("New User is connected");
 
+    socket.on("CreatedMessage", (message) => {
+        console.log("Created Message From Client", message);
+        io.emit("newMessage",{
+            text:message.text,
+            from:message.from
+        });
+    });
 
-socket.on("disconnect", () => {
-    console.log("User is disconnected");
+    socket.on("disconnect", () => {
+        console.log("User is disconnected");
+    });
 });
-});
 
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Server is on ${port}`);
 })
