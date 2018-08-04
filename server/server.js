9c888ceadc32fff1780ea4daf9fffb905399f251
janardan96 <jdcodemon@gmail.com>
2018-08-04 23:13:44 +0530

    Test case

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const socketIO = require("socket.io");
 const http = require("http");
 
+const {generateMessage}=require("./utlis/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000
 var app = express();
@@ -14,13 +15,20 @@ app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
     console.log("New User is connected");
+    
+    socket.emit("newMessage", generateMessage("janardan","Welcome to the chat room"));
+
+    
+        socket.broadcast.emit("newMessage",generateMessage("Admin","New User is added"));
 
     socket.on("CreatedMessage", (message) => {
         console.log("Created Message From Client", message);
-        io.emit("newMessage",{
-            text:message.text,
-            from:message.from
-        });
+        io.emit("newMessage", generateMessage(message.form,message.text));
+
+        // socket.broadcast.emit("newMessage",{
+        //     text:message.text,
+        //     from:message.from
+        // });
     });
 
     socket.on("disconnect", () => {
